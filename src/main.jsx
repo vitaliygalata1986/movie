c6e9ae058db5878e10aadce4bf1fefa0060f8dc5
vitaliy <nertis44@gmail.com>
2024-06-04 14:59:03 +0300

    fihish project

--- src/main.jsx
+++ src/main.jsx
@@ -1,33 +1,38 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import Home from "./pages/Home/Home";
-import Selected from "./pages/Selected/Selected";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Home from './pages/Home/Home';
+import Selected from './pages/Selected/Selected';
 //
-import Mainlayout from "./layout/MainLayout";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Mainlayout from './layout/MainLayout';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 // style css
-import "./index.css";
+import './index.css';
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <Mainlayout />,
+      children: [
+        {
+          index: true,
+          element: <Home />,
+        },
+
+        // movie id
+        {
+          path: '/selectedmovie/:id',
+          element: <Selected />,
+        },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: <Mainlayout />,
-    children: [
-      {
-        index: true,
-        element: <Home />,
-      },
+    basename: '/movie/',
+  }
+);
 
-      // movie id
-      {
-        path: "/selectedmovie/:id",
-        element: <Selected />,
-      },
-    ],
-  },
-]);
-
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <RouterProvider router={router} />
 );
