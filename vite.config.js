21390163d9aebadecff46d2bd38410f6dc1a26a3
Student <azaryanmail@gmail.com>
2024-06-04 13:40:55 +0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/", // YOUR REPO NAME HERE
+  base: "/my_book/", // YOUR REPO NAME HERE
 });
